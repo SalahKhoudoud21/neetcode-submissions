54735184c99fdcbb5f2fc9713d30376846ff7140
SalahKhoudoud21 <salah.khoudoud@icloud.com>
2026-09-15 16:14:25 -0400

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.py
@@ -0,0 +1,18 @@
+import heapq
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # remember that heapq.heappop() pops heap[0] unlike regular pop() which pops last element
+        heap = []
+        maxi = []
+        for i in range(len(nums)):
+            heapq.heappush(heap, (-1*nums[i], i))
+            
+            if i >= k - 1:
+                while heap[0][1] <= i - k:
+                    heapq.heappop(heap)
+                val, index = heap[0]
+                maxi.append(-1*val)
+        return maxi
+            
+
